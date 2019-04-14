3bb4280b57eceb0723cc696f9dc21e98462038d2
OpUs-Nebula <opus.nbula@gmail.com>
2019-04-14 07:58:04 +0200

    Inference code added

--- runningTimeTests.py
+++ runningTimeTests.py
@@ -2,11 +2,13 @@ import time
 import random
 import pandas as pd
 import numpy as np
-from StatisticsHelper import PlotHelper as pH
+from statisticshelper import PlotHelper as pH
+from datacleaning import *
 from algorithms.sort import merge_sort, insertion_sort
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import MinMaxScaler
 
 #Separating graphs between sorting/training might be helpful, as generating enough datapoints for neural networks will
 #Be much more tedious than for sorting algorithms.
@@ -38,74 +40,64 @@ def sorting_test():
     sortPlot.finalize()
 
 #Neural network graph generation
-
-"""
-Note: 
-
-Might be worth packaging clean methods in seperate module
-"""
-def clean_ensamble(df, columns):
-    fncmap = {"Size":clean_sizepref,"Current Ver":clean_ver ,"Reviews":int}
-    for mapped in fncmap.keys():
-        if mapped in columns:
-            df[mapped] = df[mapped].apply(fncmap[mapped])
-    return df.dropna()
-
-def clean_sizepref(x):
-    sep = list(x)
-    pref = sep.pop()
-    consts = {"M":1, "k":0.001}
-    if pref in ["M","k"]:
-        fin = consts[pref]*float("".join(sep))
-    else:
-        fin = 0
-    return fin
-
-def clean_ver(x):
-    ret = np.nan
-    if not isinstance(x, float):
-        comps = list(x) if x != ret else ["V"]
-        if comps[0].isdigit():
-            trunc = []
-            decSep = True
-            iterN = 0
-            end = False
-            while iterN<len(comps) and not end:
-                cand = comps[iterN]
-                if cand.isdigit(): 
-                    trunc.append(cand)
-                else:
-                    if decSep:
-                        trunc.append(".")
-                        decSep = False
-                    else:
-                        if not cand in [".", ","]:
-                            end = True
-                iterN+=1
-            ret = float("".join(trunc))
-    return ret
-
 """
 Notes:
 - X = training time, Y = accuracy
 - Plot training acc vs evaluation accuracy for demonstration of overfitting
 - Layer depth variation? mebe
+- https://stackoverflow.com/questions/41308662/how-to-tune-a-mlpregressor
+- For loop cutting down training sample for each run?
 Currently clean fnc mapped(or don't need):
 Size, Current Ver, Reviews
-
 """
-def neural_inference_test(input_fields):
+
+def train_predict_ratings (X_Trn, Y_Trn, X_Val, Y_Val):
+    model = MLPRegressor()
+    model.fit(X_Trn, Y_Trn)   
+    Prds = model.predict(X_Val)
+
+    return Prds
+
+
+
+def norm_df(target):
+    return (target - target.min()) / (target.max() - target.min())
+
+def inverse_df(origin, target):
+    return (target * (origin.max() - origin.min())) + origin.min()
+
+def neural_inference_test(input_fields, verbose=False):
     appData = pd.read_csv("googleplaystore.csv",parse_dates=True, thousands="k")
-    appData = clean_ensamble(appData.drop(appData.index[10472]),input_fields) #Faulty row, had megabyte spec in reviews.
+    appData = clean_ensamble(appData.drop(appData.index[10472]),input_fields) #Faulty row, had megabyte spec in reviews
+    appData["Rating"] = pd.to_numeric(appData["Rating"])
     
-    X = appData[input_fields]
-    Y = pd.to_numeric(appData["Rating"])
-    print(X.shape)
-    print(X.info)
-    print(X.dtypes)
-    print(Y.dtypes)
+    norm_columns = input_fields+["Rating"]
+    normalized_appData = norm_df(appData[norm_columns]) #Neural network sensetive to normalization
+
+    print("Dataset:")
+    print(appData[norm_columns])
+
+    X = normalized_appData[input_fields]
+    Y = normalized_appData["Rating"]
+
+    train_X, val_X, train_y, val_y = train_test_split(X, Y, random_state = 0)
+    
+    norm_pred = train_predict_ratings(train_X, train_y, val_X, val_y)
+
+    ground_truth = inverse_df(appData["Rating"],pd.DataFrame(val_y, columns=["Rating"]))
+    predictions = inverse_df(appData["Rating"],pd.DataFrame(norm_pred, columns=["Rating"])) 
+    mae = mean_absolute_error(ground_truth, predictions)
+
+    if verbose:
+        print("To be predicted:")
+        print(ground_truth)
+
+        print("Resulting predictions:")
+        print(predictions)
+
+    print("Mean Absolute Error is: {}".format(mae))
 
 #Might be worthwhile to add parameter for Y to test aswell
 #Might add last updated if algorithm for calculating time from data gathering to last updated is easy enough
 neuralInput = ["Size","Current Ver","Reviews"]
-neural_inference_test(neuralInput)
+neural_inference_test(neuralInput,verbose=True)
