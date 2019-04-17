13937d52ed484662886ded795f590bd0bb199f13
OpUs-Nebula <opus.nbula@gmail.com>
2019-04-17 18:47:52 +0200

    NN depth test added

--- runningTimeTests.py
+++ runningTimeTests.py
@@ -52,8 +52,8 @@ Currently clean fnc mapped(or don't need):
 Size, Current Ver, Reviews
 """
 
-def train_predict_ratings (X_Trn, Y_Trn, X_Val):
-    model = MLPRegressor()
+def train_predict_ratings (X_Trn, Y_Trn, X_Val, layers):
+    model = MLPRegressor(hidden_layer_sizes=layers)
     model.fit(X_Trn, Y_Trn)   
     Prds = model.predict(X_Val)
 
@@ -65,6 +65,13 @@ def norm_df(target):
 def inverse_df(origin, target):
     return (target * (origin.max() - origin.min())) + origin.min()
 
+#def denorm_pred_plot(shard_limit,plothelper):
+#        norm_pred = train_predict_ratings(train_X.iloc[0:sample_size], train_y.iloc[0:sample_size], val_X)
+#        ground_truth = inverse_df(appData["Rating"],pd.DataFrame(val_y, columns=["Rating"]))
+#        predictions = inverse_df(appData["Rating"],pd.DataFrame(norm_pred, columns=["Rating"])) 
+#        mae = mean_absolute_error(ground_truth, predictions)
+#        plothelper.addDataPoint(sample_size, mae)
+
 def neural_inference_test(input_fields, verbose=False):
     appData = pd.read_csv("googleplaystore.csv",parse_dates=True, thousands="k")
     appData = clean_ensamble(appData.drop(appData.index[10472]),input_fields) #Faulty row, had megabyte spec in reviews
@@ -73,48 +80,45 @@ def neural_inference_test(input_fields, verbose=False):
     norm_columns = input_fields+["Rating"]
     normalized_appData = norm_df(appData[norm_columns]) #Neural network sensetive to normalization
 
-    #print("Dataset:")
-    #print(appData[norm_columns])
-
     X = normalized_appData[input_fields]
     Y = normalized_appData["Rating"]
 
     train_X, val_X, train_y, val_y = train_test_split(X, Y, random_state = 0)
-
-    #if verbose:
-    #    train_size = train_X.shape
-    #    print("Shape training sample: x={}, y={}".format(train_size[0],train_y.shape[0]))
-    #    print(train_X.iloc[train_size[0]-1])
     
-    accPlot = pH("sample size","mean absolute error")
-
-    for sample_size in range(1,train_X.shape[0]+1, 20):
-        norm_pred = train_predict_ratings(train_X.iloc[0:sample_size], train_y.iloc[0:sample_size], val_X)
+    def denorm_pred_plot(plothelper, layer_depth=1, sample_size=1600):
+        norm_pred = train_predict_ratings(train_X.iloc[0:sample_size], train_y.iloc[0:sample_size], val_X, (100,)*layer_depth)
         ground_truth = inverse_df(appData["Rating"],pd.DataFrame(val_y, columns=["Rating"]))
         predictions = inverse_df(appData["Rating"],pd.DataFrame(norm_pred, columns=["Rating"])) 
         mae = mean_absolute_error(ground_truth, predictions)
-        accPlot.addDataPoint(sample_size, mae)
-        if sample_size % 1 == 0:
-            print("Current sample size being evaluated: {}".format(sample_size))
+        plothelper.addDataPoint(sample_size, mae)
+
+    #Sample size vs MAE graph generation
+    accPlot = pH("sample size","mean absolute error")
+    
+    #Observed sample size for maximum accuracy: 1500-2000(larger and oscillations around ~0.38 remain constant)
+    #Might be worth seperating depth/sample test to different 
+    for sample_size in range(1,train_X.shape[0]+1, 20):
+        denorm_pred_plot(accPlot,sample_size=sample_size)
+        print("Current sample size being evaluated: {}".format(sample_size))
 
     accPlot.plot()
     accPlot.finalize()
-    
-#Moved into for loop:
-    #norm_pred = train_predict_ratings(train_X, train_y, val_X)
-    #ground_truth = inverse_df(appData["Rating"],pd.DataFrame(val_y, columns=["Rating"]))
-    #predictions = inverse_df(appData["Rating"],pd.DataFrame(norm_pred, columns=["Rating"])) 
-    #mae = mean_absolute_error(ground_truth, predictions)
 
+    #Network depth vs mae graph generation:
+    depthPlot = pH("layer depth","mean absolute error")
+
+    for depth in range(1,40):
+        denorm_pred_plot(depthPlot,layer_depth=depth)
+        print("layer depth being evaluated: {}".format(depth))
+
+    depthPlot.plot()
+    depthPlot.finalize()
     #if verbose:
         #print("To be predicted:")
         #print(ground_truth)
 
         #print("Resulting predictions:")
         #print(predictions)
-
-    #To be replaced by graph
-    #print("Mean Absolute Error is: {}".format(mae))
 
 #Might be worthwhile to add parameter for Y to test aswell
 #Might add last updated if algorithm for calculating time from data gathering to last updated is easy enough
