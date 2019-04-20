01203d774b7cc355cab1f229d32a4323e72f0638
Mbwenga Maliti <mbwenga.maliti@digitalroute.com>
2019-04-20 11:44:02 +0200

    Seperate figures for each test added, slight code refactor

--- StatisticsHelper.py
+++ StatisticsHelper.py
@@ -4,30 +4,37 @@ import matplotlib.pyplot as mp
 """
 Helper for decoupling plotting functionality from main sorting and training script.
 """
+
+#Added: Prints at set intervals in script
 class PlotHelper:
-	def __init__(self,xlabel,ylabel):
-		self.x = []
-		self.y = []
-		self.xlabel=xlabel
-		self.ylabel=ylabel
-		self.domain = None
+    def __init__(self,xlabel,ylabel,figInd):
+        self.x = []
+        self.y = []
+        self.xlabel=xlabel
+        self.ylabel=ylabel
+        self.domain = None
+        mp.figure(figInd)
 
-	def addDataPoint(self,n,time):
-		self.x.append(n)
-		self.y.append(time)
+    def addDataPoint(self,n,time):
+        self.x.append(n)
+        self.y.append(time)
 
-	def plot(self):
-		if not self.domain:
-			self.domain = len(self.x) #arbitrary, could also be y
-		else:
-			if not self.domain<len(self.x) or not self.domain<len(self.y):
-				print("OBS!_____________There must be a Y-value for each X-Value._____________OBS!")
+    def plot(self):
+        print("Plotting function...")
+        if not self.domain:
+            self.domain = len(self.x) #arbitrary, could also be y
+        else:
+            if not self.domain<len(self.x) or not self.domain<len(self.y):
+                print("OBS!_____________There must be a Y-value for each X-Value._____________OBS!")
+        print("Labeling axes...")
+        mp.xlabel(self.xlabel)
+        mp.ylabel(self.ylabel)
+        print("Labeling axes done.")
+        mp.plot(self.x,self.y)
+        self.x = []
+        self.y = []
+        print("Function plot done.")
 
-		mp.xlabel(self.xlabel)
-		mp.ylabel(self.ylabel)
-		mp.plot(self.x,self.y)
-		self.x = []
-		self.y = []
-
-	def finalize(self):
-		mp.show()
+    def finalize(self):
+        print("Showing graph...")
+        mp.show()
--- runningTimeTests.py
+++ runningTimeTests.py
@@ -2,7 +2,7 @@ import time
 import random
 import pandas as pd
 import numpy as np
-from statisticshelper import PlotHelper as pH
+from StatisticsHelper import PlotHelper as pH
 from datacleaning import *
 from algorithms.sort import merge_sort, insertion_sort
 from sklearn.metrics import mean_absolute_error
@@ -10,9 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import MinMaxScaler
 
-#Separating graphs between sorting/training might be helpful, as generating enough datapoints for neural networks will
-#Be much more tedious than for sorting algorithms.
-class TestHelper:
+class SortHelper:
     def __init__(self,sample_size):
         self.unordered = self.generate_lists(sample_size)
         self.algorithms = []
@@ -23,10 +21,21 @@ class TestHelper:
     def queue_sort(self,func):
         self.algorithms.append(func)
 
+class FigureIterator:
+    def __init__(self):
+        self.CurrentFig = 0
+
+    def increment_ret(self):
+        self.CurrentFig+=1
+        return self.CurrentFig
+
+FigIter = FigureIterator()
+
 #Sorting graph generation
 def sorting_test():
-    sortPlot = pH("input size","running time(s)")
-    test = TestHelper(1000)
+    print("Running sorting test...")
+    sortPlot = pH("input size","running time(s)",FigIter.increment_ret())
+    test = SortHelper(1000)
     test.queue_sort(merge_sort)
     test.queue_sort(insertion_sort)
 
@@ -38,17 +47,15 @@ def sorting_test():
             sortPlot.addDataPoint(len(array),duration)
         sortPlot.plot()
     sortPlot.finalize()
+    print("Sorting test done.")
 
 #Neural network graph generation
 """
 Notes:
-- X = depth/sample size(separate graphs), Y = accuracy
-- Plot training acc vs evaluation accuracy for demonstration of overfitting
-- Layer depth variation? mebe
 - https://stackoverflow.com/questions/41308662/how-to-tune-a-mlpregressor
 - https://stats.stackexchange.com/questions/222883/why-are-neural-networks-becoming-deeper-but-not-wider
 - For loop cutting down training sample for each run?
-Currently clean fnc mapped(or don't need):
+DataFrame columns Currently clean fnc mapped(or don't need):
 Size, Current Ver, Reviews
 """
 
@@ -65,14 +72,8 @@ def norm_df(target):
 def inverse_df(origin, target):
     return (target * (origin.max() - origin.min())) + origin.min()
 
-#def denorm_pred_plot(shard_limit,plothelper):
-#        norm_pred = train_predict_ratings(train_X.iloc[0:sample_size], train_y.iloc[0:sample_size], val_X)
-#        ground_truth = inverse_df(appData["Rating"],pd.DataFrame(val_y, columns=["Rating"]))
-#        predictions = inverse_df(appData["Rating"],pd.DataFrame(norm_pred, columns=["Rating"])) 
-#        mae = mean_absolute_error(ground_truth, predictions)
-#        plothelper.addDataPoint(sample_size, mae)
-
 def neural_inference_test(input_fields, verbose=False):
+    print("Running neural inference test...")
     appData = pd.read_csv("googleplaystore.csv",parse_dates=True, thousands="k")
     appData = clean_ensamble(appData.drop(appData.index[10472]),input_fields) #Faulty row, had megabyte spec in reviews
     appData["Rating"] = pd.to_numeric(appData["Rating"])
@@ -85,42 +86,36 @@ def neural_inference_test(input_fields, verbose=False):
 
     train_X, val_X, train_y, val_y = train_test_split(X, Y, random_state = 0)
     
-    def denorm_pred_plot(plothelper, layer_depth=1, sample_size=1600):
+    def pred_denorm(layer_depth=1, sample_size=1600):
         norm_pred = train_predict_ratings(train_X.iloc[0:sample_size], train_y.iloc[0:sample_size], val_X, (100,)*layer_depth)
         ground_truth = inverse_df(appData["Rating"],pd.DataFrame(val_y, columns=["Rating"]))
         predictions = inverse_df(appData["Rating"],pd.DataFrame(norm_pred, columns=["Rating"])) 
-        mae = mean_absolute_error(ground_truth, predictions)
-        plothelper.addDataPoint(sample_size, mae)
+        return mean_absolute_error(ground_truth, predictions)
 
     #Sample size vs MAE graph generation
-    accPlot = pH("sample size","mean absolute error")
+    accPlot = pH("sample size","mean absolute error",FigIter.increment_ret())
     
     #Observed sample size for maximum accuracy: 1500-2000(larger and oscillations around ~0.38 remain constant)
     #Might be worth seperating depth/sample test to different 
     for sample_size in range(1,train_X.shape[0]+1, 20):
-        denorm_pred_plot(accPlot,sample_size=sample_size)
         print("Current sample size being evaluated: {}".format(sample_size))
+        accPlot.addDataPoint(sample_size,pred_denorm(sample_size=sample_size))
 
     accPlot.plot()
     accPlot.finalize()
 
     #Network depth vs mae graph generation:
-    depthPlot = pH("layer depth","mean absolute error")
+    depthPlot = pH("layer depth","mean absolute error",FigIter.increment_ret())
 
     for depth in range(1,40):
-        denorm_pred_plot(depthPlot,layer_depth=depth)
         print("layer depth being evaluated: {}".format(depth))
+        depthPlot.addDataPoint(depth, pred_denorm(layer_depth=depth))
 
     depthPlot.plot()
     depthPlot.finalize()
-    #if verbose:
-        #print("To be predicted:")
-        #print(ground_truth)
 
-        #print("Resulting predictions:")
-        #print(predictions)
+    print("Neural inference test done.")
 
-#Might be worthwhile to add parameter for Y to test aswell
-#Might add last updated if algorithm for calculating time from data gathering to last updated is easy enough
+sorting_test()
 neuralInput = ["Size","Current Ver","Reviews"]
 neural_inference_test(neuralInput,verbose=True)
